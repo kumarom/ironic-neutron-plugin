514c7abd8c88c18c0f65d0440c783a214a308f7c
Jim Rollenhagen <jim@jimrollenhagen.com>
2014-05-07 10:47:33 -0700

    Fix typo

--- ironic_neutron_plugin/extensions/switch.py
+++ ironic_neutron_plugin/extensions/switch.py
@@ -69,7 +69,7 @@ class SwitchController(wsgi.Controller):
             body = body.pop("switch")
         except KeyError:
             raise exc.BadRequest(
-                resource="swtich",
+                resource="switch",
                 reason="'switch' not found in request body")
 
         try:
